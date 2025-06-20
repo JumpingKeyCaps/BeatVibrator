2aa12bad5522c9c9677b69432b66a4838f515bad
Samuel Lebailly <sam.meusa@gmail.com>
2025-06-20 20:56:59 +0200

    feature(domain/dsp): Add basic structure for the OnsetDetector.

---
+++ app/src/main/java/com/lebaillyapp/beatvibrator/domain/dsp/OnsetDetector.kt
@@ -0,0 +1,14 @@
+package com.lebaillyapp.beatvibrator.domain.dsp
+
+/**
+ * Détecteur d'onsets (transitoires)
+ */
+class OnsetDetector {
+    /**
+     * Détecte les positions d'onsets dans un signal
+     */
+    fun detectOnsets(samples: FloatArray): List<Int> {
+        // TODO: implémenter la détection d'onsets
+        return emptyList()
+    }
+}
