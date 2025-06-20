c4f7c2fd9cbaf5c424c1ca1edbec8969eb86e06b
Samuel Lebailly <sam.meusa@gmail.com>
2025-06-20 20:57:14 +0200

    feature(domain/dsp): Add basic structure for the RmsCalculator.

---
+++ app/src/main/java/com/lebaillyapp/beatvibrator/domain/dsp/RmsCalculator.kt
@@ -0,0 +1,14 @@
+package com.lebaillyapp.beatvibrator.domain.dsp
+
+/**
+ * Calculateur RMS (Root Mean Square) sur fenêtres audio
+ */
+class RmsCalculator {
+    /**
+     * Calcule la liste des valeurs RMS sur des fenêtres glissantes
+     */
+    fun computeRmsOverWindows(samples: FloatArray, windowSize: Int, hopSize: Int): List<Float> {
+        // TODO: implémenter le calcul RMS
+        return emptyList()
+    }
+}
