3625dee9396c0e4c1e3fa55adb8ff8ee4263117a
Samuel Lebailly <sam.meusa@gmail.com>
2025-06-20 18:09:31 +0200

    feature(di): Create an AppModule object for all the provider used by hilt to inject.

---
+++ app/src/main/java/com/lebaillyapp/beatvibrator/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.lebaillyapp.beatvibrator.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class) // scope application
+object AppModule {
+
+
+}
