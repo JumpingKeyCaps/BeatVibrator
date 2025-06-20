6e846e45ecf107a87722ab1ac8c6663246f9bc2f
Samuel Lebailly <sam.meusa@gmail.com>
2025-06-20 20:56:45 +0200

    feature(domain/dsp): Add basic structure for the FFT.

---
+++ app/src/main/java/com/lebaillyapp/beatvibrator/domain/dsp/FFT.kt
@@ -0,0 +1,14 @@
+package com.lebaillyapp.beatvibrator.domain.dsp
+
+/**
+ * FFT et spectrogramme
+ */
+class FFT {
+    /**
+     * Calcule un spectrogramme en découpant le signal en fenêtres FFT
+     */
+    fun computeSpectrogram(samples: FloatArray, fftSize: Int, hopSize: Int): List<FloatArray> {
+        // TODO: implémenter le spectrogramme
+        return emptyList()
+    }
+}
