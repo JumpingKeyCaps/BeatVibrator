3a668bd4bb2e9ea199a16c0d0050fd00f912ab3f
Samuel Lebailly <sam.meusa@gmail.com>
2025-06-20 18:05:51 +0200

    chore(build.gradle): [App] - Add Hilt and KSP plugins to the App + add dependencies hilt / ksp / foundation / ui compose / lifecycleViewmodel / hilt navigation compose.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -49,6 +51,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
+    implementation ("androidx.compose.foundation:foundation:1.8.2")
+    implementation ("androidx.compose.ui:ui:1.8.2")
+
+    //Di - Hilt ----
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    //Test Stuff ---
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
